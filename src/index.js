6000092b6bfd16e1fa6fc6241ebc2860c0dde4ad
ReginaS1987 <reginasirazieva1987@gmail.com>
2024-04-19 14:41:31 +0300

    add component CharInfo

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 
-
 import './style/style.scss';
 
- 
-
-
 ReactDOM.render(
-
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
